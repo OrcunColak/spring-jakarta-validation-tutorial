fea111e02bd7253871267c3126030ba54858fc25
Orçun Çolak <orcun.colak@hazelcast.com>
2024-01-29 20:56:37 +0300

    BindingResultConverter example

--- src/main/java/com/colak/springjakartavalidationtutorial/bindingresultcontroller/controller/UserRegistrationController.java
+++ src/main/java/com/colak/springjakartavalidationtutorial/bindingresultcontroller/controller/UserRegistrationController.java
@@ -1,7 +1,10 @@
 package com.colak.springjakartavalidationtutorial.bindingresultcontroller.controller;
 
 import com.colak.springjakartavalidationtutorial.bindingresultcontroller.dto.UserRegistrationDto;
+import com.colak.springjakartavalidationtutorial.util.BindingResultConverter;
 import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
@@ -10,9 +13,16 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+import java.util.Map;
+
 @RestController
 @RequestMapping(path = "/api/v1/userregistration/")
+@RequiredArgsConstructor
+@Slf4j
 public class UserRegistrationController {
+
+    private final BindingResultConverter bindingResultConverter;
 
     /**
      * The controller receives BindingResult
@@ -21,6 +31,9 @@ public class UserRegistrationController {
     public ResponseEntity<String> createUser(@RequestBody @Valid UserRegistrationDto userRegistrationDto,
                                              BindingResult bindingResult) {
         if (bindingResult.hasErrors()) {
+            Map<String, List<String>> errorMap = bindingResultConverter.errorMap(bindingResult);
+            log.info("Error Map : {}", errorMap);
+
             return ResponseEntity.badRequest().body("Validation failed");
         }
         return ResponseEntity.ok("User created successfully");
