de16aa8ecb872df5527a6510baeb67b8781d2a68
Orçun Çolak <orcun.colak@hazelcast.com>
2024-11-29 13:19:34 +0300

    use locale

--- src/main/java/com/colak/springtutorial/dto/UserRegistrationDto.java
+++ src/main/java/com/colak/springtutorial/dto/UserRegistrationDto.java
@@ -13,6 +13,10 @@ public class UserRegistrationDto {
     @NotBlank(message = "{user.username.blank}")
     private String username;
 
+    // The @Size annotation in Java validation does not handle blank (empty) values by default.
+    // It only checks for the length of the string (or the size of a collection or array), and it doesn't validate whether
+    // the string is non-empty or contains non-whitespace characters.
+    @NotBlank(message = "{user.password.size}")
     @Size(min = 8, message = "{user.password.size}")
     private String password;
 
--- src/test/java/com/colak/springtutorial/annotationcontroller/UserRegistrationController2Test.java
+++ src/test/java/com/colak/springtutorial/annotationcontroller/UserRegistrationController2Test.java
@@ -6,11 +6,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.ProblemDetail;
 
 import java.util.Map;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class UserRegistrationController2Test {
@@ -44,5 +46,38 @@ class UserRegistrationController2Test {
         assert properties != null;
 
         assertEquals("Please provide a username", properties.get("username"));
+        assertEquals("Password must be at least 8 characters long", properties.get("password"));
+    }
+
+    @Test
+    void testInvalidFrench() {
+        UserRegistrationDto userRegistrationDto = new UserRegistrationDto();
+
+        // Test for French locale by changing Accept-Language header
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Accept-Language", "fr"); // Change to French locale
+        HttpEntity<UserRegistrationDto> requestWithFrenchLocale = new HttpEntity<>(userRegistrationDto, headers);
+
+        ProblemDetail result = testRestTemplate.exchange(url, HttpMethod.POST, requestWithFrenchLocale, ProblemDetail.class).getBody();
+        Map<String, Object> properties = result.getProperties();
+        assert properties != null;
+        assertEquals("Veuillez fournir un nom d'utilisateur", properties.get("username"));
+        assertEquals("Le mot de passe doit contenir au moins 8 caractères", properties.get("password"));
+    }
+
+    @Test
+    void testInvalidGerman() {
+        UserRegistrationDto userRegistrationDto = new UserRegistrationDto();
+
+        // Test for German locale by changing Accept-Language header
+        HttpHeaders headers = new HttpHeaders();
+        headers.add(HttpHeaders.ACCEPT_LANGUAGE, "de"); // Change to German locale
+        HttpEntity<UserRegistrationDto> requestWithGermanLocale = new HttpEntity<>(userRegistrationDto, headers);
+
+        ProblemDetail result = testRestTemplate.exchange(url, HttpMethod.POST, requestWithGermanLocale, ProblemDetail.class).getBody();
+        Map<String, Object> properties = result.getProperties();
+        assert properties != null;
+        assertEquals("Bitte geben Sie einen Benutzernamen an", properties.get("username"));
+        assertEquals("Das Passwort muss mindestens 8 Zeichen lang sein", properties.get("password"));
     }
 }
